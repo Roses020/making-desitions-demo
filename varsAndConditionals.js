0e030770d63fa12f770c21e1b2dd22206000b88b
aimee ray <aimeeray3@gmail.com>
2022-10-19 13:17:27 -0600

    finishdemo

---
+++ varsAndConditionals.js
@@ -0,0 +1,45 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+
+
+let kenobiAttack = 25
+let skywalkerAttack = 35
+
+if(kenobiAttack > skywalkerAttack){
+    console.log('Obiwan has a better attack')
+} else if(kenobiAttack < skywalkerAttack) {
+    console.log('Anakin has the better attack')
+} else {
+    console.log('Obiwan and Anakin have the same attack')
+}
+
+let kenobiHealth = 100
+let kenobiDefense = 0
+
+if(skywalkerAttack >= kenobiHealth) {
+    console.log('Obiwan was slain')
+} else {
+    kenobiHealth = kenobiHealth - skywalkerAttack
+    //kenobiHealth -= skywalkerAttack
+    console.log("Obiwan's health is now " + kenobiHealth)
+}
+
+kenobiDefense += 30
+
+if ((kenobiHealth + 50) >= 100){
+    kenobiHealth = 100
+} else {
+     kenobiHealth += 50
+}
+
+for(let i = 0; i < 5; i++){
+    kenobiHealth -= (skywalkerAttack - kenobiDefense)
+    console.log(`Obiwan's health is now ${kenobiHealth}`)
+}
+
+
+
+
